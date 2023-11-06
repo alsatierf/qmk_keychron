40bed314ec5ef25f508bb6ca3e7ea2aa747441d5
Anderson Freitas <alsatierf@gmail.com>
2023-11-06 00:48:17 -0500

    [k3_pro] Mimic same behavior as in my AnnePro2

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/alsatierf/keymap.c
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/alsatierf/keymap.c
@@ -52,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     LGUI_T(KC_E),     KC_R,     KC_T,     KC_Y,     KC_U,     LGUI_T(KC_I),     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-     RALT_T(KC_ESC), KC_A, LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F),     KC_G,     KC_H,     LCTL_T(KC_J), LSFT_T(KC_K), LALT_T(KC_L),     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-     SC_LSPO,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            SC_RSPC,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                LT(WIN_FN, KC_SPC),                     KC_RALT,  KC_TRNS,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     LT(WIN_FN, KC_CAPS), KC_A, LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F),     KC_G,     KC_H,     LCTL_T(KC_J), LSFT_T(KC_K), LALT_T(KC_L),     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                     KC_RALT,  KC_TRNS,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_ansi_84(
      KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  RGB_TOG,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_CAPS,  KC_TRNS,  KC_TRNS,  OSM(MOD_LGUI),  KC_TRNS,  RGB_SPI,  KC_TRNS,  KC_HOME, KC_UP, KC_END, KC_DEL,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  OSM(MOD_LGUI),  KC_TRNS,  RGB_SPI,  KC_TRNS,  KC_HOME, KC_UP, KC_END, KC_DEL,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,  OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL),  KC_TRNS,  KC_TRNS,  KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,  KC_TRNS,            KC_TRNS,            KC_TRNS,
      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP, KC_PGDN,  KC_TRNS,  LLOCK,            KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
@@ -93,10 +93,7 @@ bool achordion_chord(uint16_t tap_hold_keycode,
 
 uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
   switch (tap_hold_keycode) {
-    case LT(WIN_FN, KC_SPC):
-    case RALT_T(KC_ESC):
-    case SC_LSPO:
-    case SC_RSPC:
+    case LT(WIN_FN, KC_CAPS):
       return 0;  // Bypass Achordion for these keys.
   }
   return 800;
@@ -120,8 +117,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case LSFT_T(KC_K):
     case LCTL_T(KC_J):
       return 250;
-    case LT(WIN_FN, KC_SPC):
-      return 130;
     default:
       return TAPPING_TERM;
   }
@@ -129,8 +124,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(WIN_FN, KC_SPC):
-            return true;
         default:
             return false;
     }
